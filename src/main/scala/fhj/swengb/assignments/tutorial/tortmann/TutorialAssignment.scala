a96945e7c20c879c477719e36e3b88616d5762df
tortmann <thomas.ortmann@edu.fh-joanneum.at>
2015-10-28 01:38:28 +0100

    test

--- src/main/scala/fhj/swengb/assignments/tutorial/tortmann/TutorialAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/tutorial/tortmann/TutorialAssignment.scala
@@ -1,4 +1,4 @@
-package fhj.swengb.assignments.tutorial.rladstaetter
+package fhj.swengb.assignments.tutorial.tortmann
 
 object TutorialAssignment {
   def hello() = "Hello world!"
--- src/test/scala/fhj/swengb/assignments/tutorial/tortmann/TutorialAssignmentTest.scala
+++ src/test/scala/fhj/swengb/assignments/tutorial/tortmann/TutorialAssignmentTest.scala
@@ -1,4 +1,4 @@
-package fhj.swengb.assignments.tutorial.rladstaetter
+package fhj.swengb.assignments.tutorial.tortmann
 
 import org.junit.Assert._
 import org.junit.Test
